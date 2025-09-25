e28331ba2964d11739f178f213767fcc629ce3df
vipins702 <vipins702@gmail.com>
2025-09-25 22:17:03 +0530

    Fix TypeScript errors in CosmicBackground component

--- src/components/CosmicBackground.tsx
+++ src/components/CosmicBackground.tsx
@@ -1,18 +1,6 @@
-import React, { useEffect, useRef } from 'react';
-
-interface FloatingOrb {
-  id: number;
-  x: number;
-  y: number;
-  size: number;
-  color: string;
-  speed: number;
-  direction: number;
-}
+import React from 'react';
 
 const CosmicBackground: React.FC = () => {
-  const canvasRef = useRef<HTMLDivElement>(null);
-
   return (
     <div className="cosmic-background">
       <div className="floating-orb"></div>
